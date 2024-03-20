00e8f9355f98888f850328b04c3981e1578b7682
gitsil10 <105307487+gitsil10@users.noreply.github.com>
2024-03-20 18:14:44 -0400

    add python data processor

---
+++ py/src/main.py
@@ -0,0 +1,18 @@
+"""
+@gitsil10
+@file main.py
+@brief application driver
+@details A file to run the application
+@version 0.1
+@date 2024-03-20
+"""
+#imports
+from utils.process_data import ProcessData
+
+#main
+def main():
+    pass
+
+#driver
+if __name__ == "__main__":
+    main()
