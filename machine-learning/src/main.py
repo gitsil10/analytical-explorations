3e2ca223e411a43453e072ec8fc65f72509a8631
gitsil10 <105307487+gitsil10@users.noreply.github.com>
2024-03-21 08:30:06 -0400

    update machine learning main

---
+++ machine-learning/src/main.py
@@ -0,0 +1,17 @@
+"""
+@gitsil10
+@version 0.1
+@date 2024-03-21
+@file main.py
+@brief application driver
+@details A file to run the application
+"""
+#imports
+
+#main
+def main():
+    pass
+
+#driver
+if __name__ == "__main__":
+    main()
